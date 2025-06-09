9824277baba259c18e4e51e0bdea8775da049ff8
CasMonDavid <dcastro_21@alu.uabcs.mx>
2025-06-08 19:57:20 -0700

    Back: middleware listo y login listo

--- Backend/Controladores/usuarioControlador.js
+++ Backend/Controladores/usuarioControlador.js
@@ -1,5 +1,6 @@
 const Usuario = require('../Modelos/usuario');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
     try {
@@ -31,4 +32,33 @@ const createUser = async (req, res) => {
     };
 };
 
-module.exports = { createUser };
+const loginUser = async (req, res) => {
+    try {
+        const { email, contrasenna } = req.body;
+    
+        if (!email || !contrasenna) return res.status(400).json({ message: "Todos los campos son obligatorios." });
+    
+        const user = await Usuario.findOne({ email });
+        if (!user) return res.status(409).json({ message: "Usuario no encontrado." });
+    
+        const isMatch = await bcrypt.compare(contrasenna, user.contrasenna);
+        if (!isMatch) return res.status(401).json({ message: "Contraseña incorrecta" });
+    
+        const token = jwt.sign(
+          { userId: user._id, nombre: user.nombre },
+          process.env.JWT_SECRET,
+          { expiresIn: "1d" } // el token durará 1 día
+        );
+    
+        res.json({ message: "Login exitoso", token });
+    } catch (err) {
+        console.error("Error en loginUser:", err);
+        res.status(500).json({ message: "Error del servidor", error: err });
+    }
+};
+
+module.exports =
+{
+    createUser, 
+    loginUser 
+};
